98cc9a2b87dcfdc43cef9ef44b259ea1d6e3c4ec
AhmedAbdelaal345 <aabdelaal145@gmail.com>
2025-07-01 16:27:39 +0300

    First_repo

---
+++ lib/services/all_category_service.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class AllCategoryService {
+  Future<List<dynamic>> getAllCategory() async {
+    Response response = await get(
+      Uri.parse("https://fakestoreapi.com/products/categories"),
+    );
+    List<dynamic> data = jsonDecode(response.body);
+    return data;
+  }
+}
